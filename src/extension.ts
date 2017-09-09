c96be22f74c0ff09da85f8ced7dad2ba33088dd8
Andy Scout <andyhuzhill@gmail.com>
2017-09-10 01:24:36 +0800

    First Release

--- src/extension.ts
+++ src/extension.ts
@@ -1,29 +1,117 @@
 'use strict';
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import { ExtensionContext, window, workspace, Disposable, commands, Range, TextEditorRevealType, StatusBarItem, StatusBarAlignment } from 'vscode';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export function activate(context: ExtensionContext) {
+    let configuration = workspace.getConfiguration("typewriterScrollMode");
+    let enabled = configuration.get<boolean>("enable");
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "typewriter-mode" is now active!');
+    let controller = new TypewriterModeController(enabled);
 
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-        // The code you place here will be executed every time your command is executed
-
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
-    });
-
-    context.subscriptions.push(disposable);
+    context.subscriptions.push(controller);
 }
 
 // this method is called when your extension is deactivated
 export function deactivate() {
+}
+
+class TypewriterModeController {
+
+    private _disposable: Disposable;
+
+    private _enable: boolean;
+
+    private _statusBarItem: StatusBarItem;
+
+    constructor(enabled: boolean) {
+        this._enable = enabled;
+        if (!this._statusBarItem) {
+            this._statusBarItem = window.createStatusBarItem(StatusBarAlignment.Left);
+        }
+
+        let editor = window.activeTextEditor;
+        if (!editor) {
+            this._statusBarItem.hide();
+        } else {
+            let doc = editor.document;
+            if ((doc.languageId === 'markdown') || (doc.languageId === 'plaintext')) {
+                if (enabled) {
+                    this._statusBarItem.text = "typewriter ON";
+                } else {
+                    this._statusBarItem.text = "typewriter OFF";
+                }
+                this._statusBarItem.show();
+            }
+        }
+
+        let subscriptions: Disposable[] = [];
+
+        workspace.onDidChangeConfiguration(this._onConfigurationChanged, this, subscriptions);
+
+        window.onDidChangeTextEditorSelection(this._onTextDocumentSelectionChanged, this, subscriptions);
+
+        this._disposable = Disposable.from(...subscriptions);
+
+        commands.registerCommand("typewriterScrollMode.toggleEnabled", () => {
+            this._toggleEnabled();
+        });
+    }
+
+    dispose() {
+        this._disposable.dispose();
+        this._statusBarItem.dispose();
+    }
+
+    private _onTextDocumentSelectionChanged() {
+        let editor = window.activeTextEditor;
+        if (!this._enable || !editor) {
+            this._statusBarItem.hide();
+            return;
+        }
+
+        let doc = editor.document;
+        if ((doc.languageId === 'markdown') || (doc.languageId === 'plaintext')) {
+            let selection = editor.selection;
+
+            let range = new Range(selection.active, selection.active);
+            editor.revealRange(range, TextEditorRevealType.InCenter);
+            this._statusBarItem.show();
+        } else {
+            this._statusBarItem.hide();
+        }
+    }
+
+    private _onConfigurationChanged() {
+        let configuration = workspace.getConfiguration('typewriterScrollMode');
+
+        if (configuration.get('on')) {
+            this._enable = true;
+            this._statusBarItem.text = "Typewriter ON";
+        } else {
+            this._enable = false;
+            this._statusBarItem.text = "Typewriter OFF";
+        }
+
+        this._statusBarItem.show();
+    }
+
+    private _toggleEnabled() {
+        this._enable = !this._enable;
+
+        let configuration = workspace.getConfiguration('typewriterScrollMode');
+        configuration.update('enable', this._enable);
+
+        if (this._enable) {
+            window.showInformationMessage("Typewriter scroll mode is toggled on!");
+            this._statusBarItem.text = "Typewriter ON";
+        } else {
+            window.showInformationMessage("Typewriter scroll mode is toggled off!");
+            this._statusBarItem.text = "Typewriter OFF";
+        }
+
+        this._statusBarItem.show();
+    }
 }
